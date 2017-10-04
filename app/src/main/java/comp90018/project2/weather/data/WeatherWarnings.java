a8b9e9e1e48543cdd5ef665b42bb28c3c0c96ef8
sunxiaofeng <sunxiaofeng@outlook.com>
2017-10-04 15:41:02 +1100

    Changes on warning message

--- app/src/main/java/comp90018/project2/weather/data/WeatherWarnings.java
+++ app/src/main/java/comp90018/project2/weather/data/WeatherWarnings.java
@@ -7,6 +7,7 @@ public class WeatherWarnings {
     public static Optional<String> getWarningMessage(Item item) {
         Condition condition = item.getForecast()[0];
         int code = condition.getCode();
+        StringBuilder message = new StringBuilder();
         switch (code) {
             case 0: // Tornado
             case 1: // Tropical Storm
@@ -20,10 +21,31 @@ public class WeatherWarnings {
             case 39: // Scattered Thunderstorms
             case 45: // Thundershowers
             case 47: // Isolated Thundershowers
-                return Optional.of(condition.getDescription() + " today. Please be careful and stay indoors!");
+                message.append(condition.getDescription());
+                message.append(" today. Please be careful and stay indoors! ");
+                break;
+            case 8: // Freezing drizzle
+            case 9: // Drizzle
+            case 10: // Freezing rain
+            case 11: // Showers
+            case 12: // Showers
+            case 35: // Mixed rain and hail
+            case 40: // Scattered showers
+                message.append(condition.getDescription());
+                message.append(". Please bring an umbrella or raincoat. ");
+                break;
             default:
-                // No warning message
-                return Optional.absent();
+                break;
         }
+
+        if (condition.getHighTemperature() > 32) {
+            message.append("High temperature. Drink more water to prevent heat stroke. ");
+        } else if (condition.getLowTemperature() < 0) {
+            message.append("Low temperature. Dress warmly if you're going out. ");
+        }
+        if (message.length() > 0) {
+            return Optional.of(message.toString());
+        }
+        return Optional.absent();
     }
 }
